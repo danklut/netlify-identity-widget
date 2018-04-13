e678ad69edfae49e3a4f2a7f7448570593199afc
danklut <danklutmeyer@hotmail.ca>
2018-04-13 04:38:17 -0700

    Update siteurl.js

--- src/components/forms/siteurl.js
+++ src/components/forms/siteurl.js
@@ -42,7 +42,7 @@ export default class SiteURLForm extends Component {
           </label>
         </div>
         <button type="submit" className="btn">
-          Set site's "www.aomcloudenterprise.com"
+          Set site's "url"= "www.aomcloudenterprise.com"
       </form>
     );
   }
